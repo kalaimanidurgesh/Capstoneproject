b8cbcc2b6583d130fbd76a5713f7e182203cf66e
Kalaimani Durgesh <83447533+kalaimanidurgesh@users.noreply.github.com>
2024-08-25 19:29:58 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # Tag the image
-docker tag devops-build_web:latest kalaimanidurgesh/dev:latest
+docker tag devops-build_web:latest kalaimanidurgesh/prod:latest
 
 # Push to Docker Hub public repo
-docker push kalaimanidurgesh/dev:latest
+docker push kalaimanidurgesh/prod:latest
 
 # Pull the latest image from Docker Hub
-docker pull kalaimanidurgesh/dev:latest
+docker pull kalaimanidurgesh/prod:latest
 
 # Stop and remove the existing container if it exists
 docker stop myapp || true
 docker rm myapp || true
 
 # Run the new container
-docker run -d -p 80:80 --name myapp kalaimanidurgesh/dev:latest
+docker run -d -p 80:80 --name myapp kalaimanidurgesh/prod:latest
 
